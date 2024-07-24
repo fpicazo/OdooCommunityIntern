d87bad2efef04aa6815ef2a979d5379536d929be
fpicazo <fpicazo@infinitecmexico.com>
2024-07-24 16:19:47 -0700

    new

--- custom_bill_receive/controllers/bill_receive_controller.py
+++ custom_bill_receive/controllers/bill_receive_controller.py
@@ -5,15 +5,15 @@ from odoo.http import request
 class BillReceiveController(http.Controller):
 
     @http.route('/api/receive_bills', type='json', auth='public', methods=['POST'], csrf=False)
-    def receive_bills(self, **post):
-        bills_data = post.get('bills')
+    def receive_bills(self):
+        bills_data = request.jsonrequest.get('bills')
         if not bills_data:
             return {'error': 'No bills data received'}
 
         for bill_data in bills_data:
             request.env['account.move'].sudo().create({
                 'name': bill_data['name'],
-                'amount_total': bill_data['amount_total'], 
+                'amount_total': bill_data['amount_total'],
                 'folio_fiscal': bill_data['folio_fiscal'],
                 # Add more fields as needed
             })
