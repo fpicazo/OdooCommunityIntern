3fa4975339204942e0dc35c1dbb9950d3c0f587a
fpicazo <fpicazo@infinitecmexico.com>
2024-07-26 15:24:30 -0700

    push

--- custom_facturacion/models/__init__.py
+++ custom_facturacion/models/__init__.py
@@ -2,7 +2,6 @@ from . import res_partner
 from . import product_template
 from . import account_move
 from . import project
-from . import task
 from . import sale_order
 from . import hr_employee
 
