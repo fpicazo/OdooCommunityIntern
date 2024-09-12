b7e34115e20e0cca7863eae0773299dde71ba195
fpicazo <fpicazo@infinitecmexico.com>
2024-09-11 19:08:25 -0700

    new

---
+++ custom_facturacion copy/__manifest__.py
@@ -0,0 +1,21 @@
+# invoice_button_draft/__manifest__.py
+{
+    'name': 'Custom Facturacion Mexicana Copy',
+    'version': '1.0',
+    'summary': 'Facturacion Mexicana',
+    'author': 'Flavien Picazo',
+    'category': 'Accounting',
+    'depends': ['account', 'product','project','sale','hr'],
+    'data': [
+        'views/res_partner_views.xml',
+        'views/product_template_views.xml',
+        'views/account_move_views.xml',
+        'views/project_view.xml',
+        'views/task_view.xml',
+        'views/sale_order_view.xml',
+        'views/hr_employee_view.xml',
+        'views/res_company_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
---
+++ custom_facturacion copy/models/res.company.py
@@ -0,0 +1,30 @@
+from odoo import models, fields
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+
+    l10n_mx_edi_fiscal_regime = fields.Selection([
+        ('601', 'General de Ley de Personas Morales'),
+        ('603', 'Personas morales con fines no lucrativos'),
+        ('605', 'Sueldos y Salarios e Ingresos Asimilados a Salarios'),
+        ('606', 'Arrendamiento'),
+        ('607', 'Régimen de enajenación o adquisición de bienes'),
+        ('608', 'Demás ingresos'),
+        ('609', 'Consolidación'),
+        ('610', 'Residentes en el extranjero sin establecimiento permanente en México'),
+        ('611', 'Ingresos por Dividendos (socios y accionistas)'),
+        ('612', 'Personas físicas con actividades empresariales y profesionales'),
+        ('614', 'Ingresos por intereses'),
+        ('615', 'Régimen de los ingresos por obtención de premios'),
+        ('616', 'Sin obligaciones fiscales'),
+        ('620', 'Sociedades Cooperativas de Producción que optan por diferir sus ingresos'),
+        ('621', 'Incorporación fiscal'),
+        ('622', 'Actividades Agrícolas, Ganaderas, Silvícolas y Pesqueras'),
+        ('623', 'Opcional para grupos de sociedades'),
+        ('624', 'Coordinados'),
+        ('625', 'Régimen de las Actividades Empresariales con ingresos a través de Plataformas Tecnológicas'),
+        ('626', 'Régimen Simplificado de Confianza - RESICO'),
+        ('628', 'Hidrocarburos'),
+        ('629', 'De los Regímenes Fiscales Preferentes y de las Empresas Multinacionales'),
+        ('630', 'Enajenación de acciones en bolsa de valores'),
+    ], string='Régimen Fiscal', help="Fiscal Regime for CFDI 4.0")
