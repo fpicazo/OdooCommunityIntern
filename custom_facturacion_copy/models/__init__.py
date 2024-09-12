d88441c619470e038fc80c302096890b8edd06c6
fpicazo <fpicazo@infinitecmexico.com>
2024-09-11 19:16:38 -0700

    new

--- custom_facturacion_copy/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import res_partner
-from . import product_template
-from . import account_move
-from . import project
-from . import sale_order
-from . import hr_employee
-from . import res_company
--- custom_facturacion_copy/models/product_template.py
+++
@@ -1,8 +0,0 @@
-# invoice_button_draft/models/product_template.py
-from odoo import models, fields
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    sat_unit_code = fields.Char(string='Sat Unit Code')
-    sat_code_product = fields.Char(string='Sat Code Product')
