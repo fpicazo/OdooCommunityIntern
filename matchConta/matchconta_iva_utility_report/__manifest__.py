761ceabe2300209380f4d05fcfaebe45a1d71e2b
Flavien Picazo <fpicazo@infinitecmexico.com>
2026-06-17 17:51:31 -0700

    new Module

---
+++ matchConta/matchconta_iva_utility_report/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "MatchConta IVA Utility Report",
+    "version": "18.0.1.0.0",
+    "summary": "Monthly client/vendor payment and IVA utility report",
+    "category": "Accounting/Accounting",
+    "author": "OpenAI",
+    "license": "LGPL-3",
+    "depends": ["account"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/matchconta_iva_utility_report_views.xml",
+    ],
+    "application": False,
+    "installable": True,
+}
